d0adc58d9c6ab46e1436c94c872337990cc9ed6f
Emilia-Dumitru <dumitruemilia41@gmail.com>
2024-06-01 13:24:22 +0300

    am modificat conf recomandarilor

--- ex3.js
+++ ex3.js
@@ -2,18 +2,18 @@
 Funcția letterCount() returnează numărul de apariții al unei litere trimise ca parametru dintr-un șir dat.
 */
 
-const letterCount = (str, letter)=> {
+const letterCount = (str, letter) => {
     str = str.toLowerCase();
     let count = 0;
-        for (i=0; i < str.length; i++) {
-            const el = str[i];
-            if (letter.includes(el))
-                count ++
-        }
-    return count ;
+    for (i = 0; i < str.length; i++) {
+        const el = str[i];
+        if (letter.includes(el))
+            count++
+    }
+    return count;
 }
 
 const str = "Îmi place programarea";
 const str2 = "Vreau să lucrez în IT";
-console.log (letterCount(str, "a"));
-console.log (letterCount(str2, "u"));
+console.log(letterCount(str, "a"));
+console.log(letterCount(str2, "u"));
