7f6db27795d36247ac854f3810938716fec75f0a
Emilia-Dumitru <dumitruemilia41@gmail.com>
2024-06-01 13:31:10 +0300

    am modifc dupa recomandari

--- ex4.js
+++ ex4.js
@@ -4,11 +4,12 @@ Numărul de argumente nu este definit. Se poate transmite un număr nelimitat de
 */
 const addNumber = (...argumente) => {
     const initialValue = 0;
-    const sum = argumente.reduce((accumulator , currentValue) => accumulator + currentValue, initialValue);
+    const sum = argumente.reduce((accumulator, currentValue) => accumulator + currentValue, initialValue);
     return sum;
-}
-const args = addNumber(1,2,3);
-const args2 = addNumber(1,2,3,4,5);
-
-console.log(args); // 6
-console.log (args2); // 15
+};
+const args = addNumber(1, 2, 3);
+const args2 = addNumber(1, 2, 3, 4, 5);
+// 6
+console.log(args);
+// 15
+console.log(args2);
