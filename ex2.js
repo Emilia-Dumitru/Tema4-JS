7241a1a0a2a27083167002dd40c5e16756180cbe
Emilia-Dumitru <dumitruemilia41@gmail.com>
2024-06-01 13:20:44 +0300

    am modificat ex2 conf recomandarilor

--- ex2.js
+++ ex2.js
@@ -4,14 +4,16 @@ Aceste două variabile sunt capetele intervalului pentru numărul general: [sta
 */
 
 const getRandomNumber = (start, end) => {
-    return Math.random () * (end - start ) + start;
-}
-console.log(getRandomNumber(3, 5)); // Nu ne va da un numar intreg 
+    return Math.random() * (end - start ) + start;
+};
+// Nu ne va da un numar intreg 
+console.log(getRandomNumber(3, 5)); 
 
 const getRandomNumber2 = (start, end) => {
-    return Math.floor(Math.random () * (end - start )) + start;
-}
-console.log(getRandomNumber2(3, 5)) //Cu math floor ne va da un numar intreg 
+    return Math.floor(getRandomNumber(start, end));
+};
+ //Cu math floor ne va da un numar intreg 
+console.log(getRandomNumber2(3, 5)); 
 
 const returnRandomNum = "Acesta este un numar >= 3 si < 5: " + getRandomNumber2 (3,5);
 console.log(returnRandomNum);
